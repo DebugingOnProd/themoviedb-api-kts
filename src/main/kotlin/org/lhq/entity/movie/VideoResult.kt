d78a6c2e4401f103682379a316022e1d8db1d678
DebuggingOnProd <lhqblue@outlook.com>
2024-11-20 15:08:14 +0800

    feat(movie): 增加视频结果的序列化字段并添加日期时间相关库
    - 在 VideoItem 类中添加了 iso6391、iso31661 和 publishedAt 字段，并使用 @SerializedName 注解
    - 引入了 com.google.gson.annotations.SerializedName 注解库
    - 引入了 java.time.LocalDateTime 类，尽管在此代码中未直接使用，可能是为后续处理日期时间做准备

--- src/main/kotlin/org/lhq/entity/movie/VideoResult.kt
+++ src/main/kotlin/org/lhq/entity/movie/VideoResult.kt
@@ -1,5 +1,8 @@
 package org.lhq.entity.movie
 
+import com.google.gson.annotations.SerializedName
+import java.time.LocalDateTime
+
 data class VideoResult(
     val id: Int,
     val results: List<VideoItem>
@@ -7,7 +10,9 @@ data class VideoResult(
 
 
 data class VideoItem(
+    @SerializedName("iso_639_1")
     val iso6391: String,
+    @SerializedName("iso_3166_1")
     val iso31661: String,
     val name: String,
     val key: String,
@@ -15,6 +20,7 @@ data class VideoItem(
     val size: Int,
     val type: String,
     val official: Boolean,
+    @SerializedName("published_at")
     val publishedAt: String,
     val id: String
 )
