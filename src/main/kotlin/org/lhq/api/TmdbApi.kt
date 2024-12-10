00e848b751eea9b0b21acbf4fceb1f69146c665e
DebuggingOnProd <lhqblue@outlook.com>
2024-12-10 10:03:08 +0800

    feat(api): 添加 Trending API支持
    - 在 TmdbApi 中添加了对 TrendingApi 的支持
    - 新增 getTrendingApi() 方法
    - 实现了基本的 TrendingApi 类

--- src/main/kotlin/org/lhq/api/TmdbApi.kt
+++ src/main/kotlin/org/lhq/api/TmdbApi.kt
@@ -12,6 +12,7 @@ class TmdbApi private constructor( private val tmdbConfig: TmdbConfig) {
             AccountApi::class.java -> AccountApi(httpClient)  as T
             NetworkApi::class.java -> NetworkApi(httpClient) as T
             SearchApi::class.java -> SearchApi(httpClient) as T
+            TrendingApi::class.java -> TrendingApi(httpClient) as T
             else -> throw IllegalArgumentException("Unsupported API class: $apiClass")
         }
     }
@@ -19,8 +20,8 @@ class TmdbApi private constructor( private val tmdbConfig: TmdbConfig) {
     fun getMovieApi() = createApi(MovieApi::class.java)
     fun getAccountApi() = createApi(AccountApi::class.java)
     fun getNetworkApi() = createApi(NetworkApi::class.java)
-
     fun getSearchApi() = createApi(SearchApi::class.java)
+    fun getTrendingApi() = createApi(TrendingApi::class.java)
 
 
 
---
+++ src/main/kotlin/org/lhq/api/TrendingApi.kt
@@ -0,0 +1,7 @@
+package org.lhq.api
+
+import org.lhq.http.HttpClient
+
+class TrendingApi(private val httpClient: HttpClient) {
+
+}
